fbca2a8775e1439d1c17c80a401d7fd924a46746
Alexey Mavrin <amavrin@gmail.com>
2024-03-25 23:49:27 +0300

    FillSingleForward: clarify the task

--- vectors/FillSingleForward/FillSingleForward/Source.cpp
+++ vectors/FillSingleForward/FillSingleForward/Source.cpp
@@ -1,8 +1,12 @@
 #include "Header.h"
 
 // fill_forward() takes vector<unsigned int>
-// it checks if all elements except one are zero
+// it checks if all elements except the single one are zero
 // if not function returns 'false'
+//
+// For correct vectors if fills all elements followinf the non-zero
+// with decremened values: for example, if v is {0,3,0,0,0}
+// it transforms the vector into {0,3,2,1,0}
 bool fill_forward(vector<unsigned int>& v)
 {
 	return true;
