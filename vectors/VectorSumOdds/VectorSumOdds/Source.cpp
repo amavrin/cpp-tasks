500163679d993984c214f35e9e7cd47f2d10f2b2
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-28 20:24:18 +0300

    VectorSumOdds: task solved

--- vectors/VectorSumOdds/VectorSumOdds/Source.cpp
+++ vectors/VectorSumOdds/VectorSumOdds/Source.cpp
@@ -3,5 +3,13 @@
 // sum_odds() accumulates the sum of v's elements, whose values are odd (not divided by 2) and return the sum
 int sum_odds(vector<int> v)
 {
-	return 0;
+	int summa = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (v[i] % 2 != 0)
+		{
+			summa += v[i];
+		}
+	}
+	return summa;
 }
