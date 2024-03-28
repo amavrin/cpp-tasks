102f0481650632128db7c0cb1fe80267861fc9f5
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-28 20:05:10 +0300

    SumEvens: task solved

--- arrays/SumEvens/SumEvens/task.cpp
+++ arrays/SumEvens/SumEvens/task.cpp
@@ -1,7 +1,16 @@
 #include "task.h"
 
 // sum_evens() sums all array elements which are even, i.e. divided by 2, and returns the sum
-int sum_evens(int array[], size_t array_size) {
-	// add code here
-	return 0;
+int sum_evens(int array[], size_t array_size) 
+{
+	int summa = 0;
+
+	for (int i = 0; i < array_size; i++)
+	{
+		if (array[i] % 2 == 0)
+		{
+			summa += array[i];
+		}
+	}
+	return summa;
 }
