cc991127ae20f46a7ab9d1adb6ba3262f15dfee5
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-29 16:50:47 +0300

    SumSelected: task solved

--- arrays/SumSelected/SumSelected/task.cpp
+++ arrays/SumSelected/SumSelected/task.cpp
@@ -4,6 +4,10 @@
 // indexes of the 'array' member are set by selector's members 
 int sum_selected(int array[], int selector[], size_t selector_count)
 {
-	// place code here
-	return 0;
+	int summa = 0;
+	for (size_t i = 0; i < selector_count; i++)
+	{
+		summa += array[selector[i]]; //selector[i] используется в качестве индекса для доступа к элементам массива array.
+	}
+	return summa;
 }
