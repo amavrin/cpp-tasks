e2d9a37cdd9d22c7d7f4dd37dab7a12cb668e956
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-04-03 14:39:50 +0300

    VectorReplace: add initital version/temp

---
+++ vectors/VectorReplace/UnitTests/UnitTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\VectorReplace\Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
--- vectors/VectorReplace/VectorReplace/Source.cpp
+++ vectors/VectorReplace/VectorReplace/Source.cpp
@@ -5,5 +5,22 @@
 // function replace() returns the number of replacements made
 int replace_strings(vector<string>& data, string from, string to)
 {
-	return 0;
+    int replacements = 0;
+
+    for (int i = 0; i < data.size(); i++) 
+    {
+        size_t pos = 0;
+        bool replacedInString = false; // Добавляем флаг для отслеживания замен в строке
+        while ((pos = data[i].find(from, pos)) != string::npos) 
+        {
+            data[i].replace(pos, from.length(), to);
+            pos += to.length(); // Перемещаемся на длину замененной строки, чтобы избежать бесконечного цикла
+            replacedInString = true; // Устанавливаем флаг, что в строке была произведена замена
+        }
+        if (replacedInString) // Увеличиваем счетчик только если в строке была хотя бы одна замена
+        {
+            replacements++;
+        }
+    }
+    return replacements;
 }
