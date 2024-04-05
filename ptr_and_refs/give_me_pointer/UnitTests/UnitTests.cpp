b819bcbb04eff9ff73e42ad23094c5024b253ddb
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-04-05 20:14:07 +0300

    GiveMePointer: task solved

---
+++ ptr_and_refs/give_me_pointer/UnitTests/UnitTests.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\give_me_pointer\Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestGivePointerCorrect)
+		{
+			int a1[5]{ 1,2,3,2,1 };
+			int a2[5]{ 3,3,3,3,3 };
+
+			int* p1 = find_elem(a1, 5, 2);
+			int* p2 = find_elem(a2, 5, 2);
+
+			Assert::IsTrue(p1 == &a1[1]);
+			Assert::IsTrue(p2 == nullptr);
+		}
+		TEST_METHOD(TestGivePointerZero)
+		{
+			int a1[5]{ 1,2,3,2,1 };
+			int a2[5]{ 1,1,1,0,0 };
+
+			int* p1 = find_elem(a1, 5, 2);
+			int* p2 = find_elem(a2, 5, 0);
+
+			Assert::IsTrue(p1 == &a1[1]);
+			Assert::IsTrue(p2 == &a2[3]);
+		}
+		TEST_METHOD(TestGivePointerNegative)
+		{
+			int a1[5]{ 1,2,3,2,1 };
+			int a2[5]{ -3,3,3,3,-3 };
+
+			int* p1 = find_elem(a1, 5, 2);
+			int* p2 = find_elem(a2, 5, -3);
+
+			Assert::IsTrue(p1 == &a1[1]);
+			Assert::IsTrue(p2 == &a2[0]);
+		}
+	}; 
+}
