2a138d4f7a547f5642871ed04d0005cf526ef1c5
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-04-06 19:30:34 +0300

    GiveMePointer: fixed test-cases

--- ptr_and_refs/give_me_pointer/UnitTests/UnitTests.cpp
+++ ptr_and_refs/give_me_pointer/UnitTests/UnitTests.cpp
@@ -10,38 +10,51 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestGivePointerCorrect)
+		TEST_METHOD(TestGivePointerCorrect_NumberFound)
 		{
 			int a1[5]{ 1,2,3,2,1 };
-			int a2[5]{ 3,3,3,3,3 };
-
 			int* p1 = find_elem(a1, 5, 2);
+
+			Assert::IsTrue(p1 == &a1[1]);
+		}
+
+		TEST_METHOD(TestGivePointerCorrect_NumberNotFound)
+		{
+			int a2[5]{ 3,3,3,3,3 };
 			int* p2 = find_elem(a2, 5, 2);
 
-			Assert::IsTrue(p1 == &a1[1]);
 			Assert::IsTrue(p2 == nullptr);
 		}
-		TEST_METHOD(TestGivePointerZero)
+		TEST_METHOD(TestGivePointerZero_NumberFound)
 		{
-			int a1[5]{ 1,2,3,2,1 };
 			int a2[5]{ 1,1,1,0,0 };
-
-			int* p1 = find_elem(a1, 5, 2);
 			int* p2 = find_elem(a2, 5, 0);
 
-			Assert::IsTrue(p1 == &a1[1]);
 			Assert::IsTrue(p2 == &a2[3]);
 		}
-		TEST_METHOD(TestGivePointerNegative)
+		TEST_METHOD(TestGivePointerZero_NumberNotFound)
+		{
+			int a1[5]{ 1,1,1,1,4 };
+
+			int* p1 = find_elem(a1, 5, 0);
+
+			Assert::IsTrue(p1 == nullptr);
+		}
+		TEST_METHOD(TestGivePointerNegative_NumberFound)
+		{
+			int a1[5]{ -3,3,3,3,-3 };
+
+			int* p1 = find_elem(a1, 5, -3);
+
+			Assert::IsTrue(p1 == &a1[0]);
+		}
+		TEST_METHOD(TestGivePointerNegative_NumberNotFound)
 		{
 			int a1[5]{ 1,2,3,2,1 };
-			int a2[5]{ -3,3,3,3,-3 };
 
-			int* p1 = find_elem(a1, 5, 2);
-			int* p2 = find_elem(a2, 5, -3);
+			int* p1 = find_elem(a1, 5, -3);
 
-			Assert::IsTrue(p1 == &a1[1]);
-			Assert::IsTrue(p2 == &a2[0]);
+			Assert::IsTrue(p1 == nullptr);
 		}
 	}; 
 }
