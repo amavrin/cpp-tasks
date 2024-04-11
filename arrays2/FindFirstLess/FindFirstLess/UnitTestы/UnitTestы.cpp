38c8348192cf756b91f29e7392146396a4a05088
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-04-11 16:06:20 +0300

    FindFirstLess: task solved

---
+++ arrays2/FindFirstLess/FindFirstLess/UnitTestы/UnitTestы.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\FindFirstLess\Header.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestы
+{
+	TEST_CLASS(UnitTestы)
+	{
+	public:
+		
+		TEST_METHOD(TestFindFirstLess_Correct)
+		{
+			int ordered[]{ 10, 6, 4, 2, -3 };
+			Assert::IsTrue(find_first_less(ordered, 5, 5) == 2);
+		}
+		TEST_METHOD(TestFindFirstLess_Zero)
+		{
+			int ordered[]{ 10, 6, 4, 2, 0 };
+			Assert::IsTrue(find_first_less(ordered, 5, 1) == 4);
+		}
+		TEST_METHOD(TestFindFirstLess_Negative)
+		{
+			int ordered[]{ 10, 6, 4, 2, -3 };
+			Assert::IsTrue(find_first_less(ordered, 5, -2) == 4);
+		}
+		TEST_METHOD(TestFindFirstLess_NoMatch)
+		{
+			int ordered[]{ 10, 6, 4, 2, -3 };
+			Assert::IsTrue(find_first_less(ordered, 5, -5) == -1);
+		}
+	};
+}
